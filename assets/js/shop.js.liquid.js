457d8111d26cdaf6821bc6e3578488cdbc19bdb9
The Busta Lab <bust0037@d.umn.edu>
2021-05-20 14:02:52 -0500

    Update shop.js.liquid.js

--- assets/js/shop.js.liquid.js
+++ assets/js/shop.js.liquid.js
@@ -1,11 +1,3 @@
 //   $(document).ready(function() {
 //   $('.image-link').magnificPopup({type:'image'});
 // });
-
-$(document).ready(function() {
-  $('.template-article img').each(function() {
-      var currentImage = $(this);
-      currentImage.wrap("<a class='image-link' href='" + currentImage.attr("src") + "'</a>");
-  });
-  $('.image-link').magnificPopup({type:'image'});  
-});
