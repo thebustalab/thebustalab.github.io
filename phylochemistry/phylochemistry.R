e6d5dbcb2744b8bfae2f32b2d27bd1146d33d31a
The Busta Lab <bust0037@d.umn.edu>
2021-09-08 14:19:45 -0500

    ggtree is bioconductor

--- phylochemistry/phylochemistry.R
+++ phylochemistry/phylochemistry.R
@@ -10,7 +10,7 @@
             "dplyr",
             "gridExtra",
             "ape",
-            "ggtree",
+            # "ggtree",
             "rstatix",
             "multcompView",
             "imager",
