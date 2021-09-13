bafad859e8fb48a7f580353892ae7457f67eb463
The Busta Lab <bust0037@d.umn.edu>
2021-09-12 21:32:23 -0500

    ups

--- phylochemistry/phylochemistry.R
+++ phylochemistry/phylochemistry.R
@@ -104,6 +104,7 @@
         alaska_lake_data <- read_csv("https://thebustalab.github.io/R_For_Chemists_2/sample_data/alaska_lake_data.csv", col_types = cols())
         solvents <- read_csv("https://thebustalab.github.io/R_For_Chemists_2/sample_data/solvents.csv", col_types = cols())
         periodic_table <- read_csv("https://thebustalab.github.io/R_For_Chemists_2/sample_data/per_table.csv", col_types = cols())
+        fadb_sample <- read_csv("https://thebustalab.github.io/R_For_Chemists_2/sample_data/fadb_sample.csv", col_types = cols())
         # periodic_table_small <- read_csv("https://thebustalab.github.io/R_For_Chemists/sample_data/per_table_small.csv", col_types = cols())
         ny_trees <- read_csv("https://thebustalab.github.io/R_For_Chemists_2/sample_data/ny_trees.csv", col_types = cols())
         # ckd_data <- read_csv("https://thebustalab.github.io/R_For_Chemists/sample_data/ckd_metabolomics.csv", col_types = cols())
