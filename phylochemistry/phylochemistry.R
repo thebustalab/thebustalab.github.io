0df753a57bb533b83a0342600a43f14459a6ed44
The Busta Lab <69868875+thebustalab@users.noreply.github.com>
2023-07-27 16:08:43 -0500

    all pairs t test

--- phylochemistry/phylochemistry.R
+++ phylochemistry/phylochemistry.R
@@ -11052,29 +11052,6 @@
 
         #### anovaTestMean
 
-            anovaMean <- function(object, n, ybar, s, ..., ylabel="ylabel") {
-                ## the object argument contains the levels of the factor
-                df <- sum(n)-length(ybar)
-                sigmahat <- (sum((n-1)*s^2) / df)^.5
-
-                SS.group <- sum(n*(ybar - (ybar %*% n)/sum(n))^2)
-
-                Df <- c(length(ybar)-1, sum(n)-length(ybar))
-                Sum.of.Sq <- c(SS.group, df*sigmahat^2)
-                Mean.Sq <- c(SS.group/(length(ybar)-1), sigmahat^2)
-                F.Value <- c(Mean.Sq[1]/Mean.Sq[2], NA)
-                Pr.F <- c(1-pf(F.Value[1], Df[1], Df[2]), NA)
-                result <- list(Df=Df, "Sum of Sq"=Sum.of.Sq, "Mean Sq"=Mean.Sq,
-                     "F value"=F.Value, "Pr(F)"=Pr.F)
-                oldClass(result) <- c("anova","data.frame")
-                attr(result,"row.names") <- format(c(ylabel,"Residuals"))
-                attr(result, "heading") <-
-                c("Analysis of Variance Table\n",
-                paste("Response: ", ylabel, "\n", sep=""),
-                "Terms added sequentially (first to last)")
-                result
-            }
-
             #' anova on means and standard deviations
             #'
             #' @param category
@@ -11085,7 +11062,28 @@
             #' @examples
             #' anovaTestMean()
 
-            # https://stackoverflow.com/questions/5526922/r-t-test-from-n-mean-sd
+            anovaMean <- function(object, n, ybar, s, ..., ylabel="ylabel") {
+                ## the object argument contains the levels of the factor
+                df <- sum(n)-length(ybar)
+                sigmahat <- (sum((n-1)*s^2) / df)^.5
+                SS.group <- sum(n*(ybar - (ybar %*% n)/sum(n))^2)
+                Df <- c(length(ybar)-1, sum(n)-length(ybar))
+                Sum.of.Sq <- c(SS.group, df*sigmahat^2)
+                Mean.Sq <- c(SS.group/(length(ybar)-1), sigmahat^2)
+                F.Value <- c(Mean.Sq[1]/Mean.Sq[2], NA)
+                Pr.F <- c(1-pf(F.Value[1], Df[1], Df[2]), NA)
+                result <- list(
+                    Df=Df, "Sum of Sq"=Sum.of.Sq, "Mean Sq"=Mean.Sq,
+                    "F value"=F.Value, "Pr(F)"=Pr.F
+                )
+                oldClass(result) <- c("anova","data.frame")
+                attr(result,"row.names") <- format(c(ylabel,"Residuals"))
+                attr(result, "heading") <-
+                c("Analysis of Variance Table\n",
+                paste("Response: ", ylabel, "\n", sep=""),
+                "Terms added sequentially (first to last)")
+                result
+            }
 
             anovaTestMean <- function(category, n, mean, stdev) {
 
@@ -11103,6 +11101,56 @@
                 return(output)
 
             }
+
+        #### allPairsTTestMean
+
+            allPairsTTestMean <- function(category, mean, stdev, n, correction_method = c("BH", "holm", "bonferroni")) {
+
+            df <- data.frame(
+                category = category,
+                mean = mean,
+                stdev = stdev,
+                n = n
+            )
+
+            # Initialize list to store results
+                results <- list()
+
+            # Compute all possible combinations
+                combinations <- combn(df$category, 2)
+
+            # Loop over all combinations
+                for (i in seq(ncol(combinations))) {
+                    # Select the two groups
+                    group1 <- df[df$category == combinations[1, i],]
+                    group2 <- df[df$category == combinations[2, i],]
+
+                    # Compute standard error for each group
+                    group1_se <- group1$stdev / sqrt(group1$n)
+                    group2_se <- group2$stdev / sqrt(group2$n)
+
+                    # Compute standard error for the difference between means
+                    diff_se <- sqrt(group1_se^2 + group2_se^2)
+
+                    # Compute p-value
+                    p_value <- round((2 * pt(-abs(diff / diff_se), (group1$n + group2$n - 2))), 4)
+
+                    # Store results
+                    results[[i]] <- data.frame(
+                        group1 = group1$category,
+                        group2 = group2$category,
+                        difference_in_means = (group1$mean - group2$mean),
+                        difference_in_means_se = round(diff_se, 4),
+                        p_value = p_value
+                    )
+                }
+
+            # Check the results
+            results <- do.call(rbind, results)
+            results$p.adj <- p.adjust(results$p_value, method = correction_method[1])
+            results$signif <- ifelse(results$p.adj < 0.05, "*", "")
+            return(results)
+        }
 
     ##### Phylogenetic statistical testing
 
