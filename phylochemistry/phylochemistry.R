26233e3aa65ab36117c9b8abf054e03051e45121
The Busta Lab <69868875+thebustalab@users.noreply.github.com>
2025-09-30 09:15:26 -0500

    newsletter update

--- phylochemistry/phylochemistry.R
+++ phylochemistry/phylochemistry.R
@@ -10588,7 +10588,7 @@
                             
                             ## How to prepare blurbs
                             "You are a news reporter that specializes in summarizing the abstracts of scientific articles into short two to three sentence blurbs.",
-                            "Each blurb should begin with the name of any chemical compounds or compound classes being described in the abstract.",
+                            "Each blurb should begin with (i) the name of any chemical compounds or compound classes being described in the abstract and (ii) the name of the journal in which the abstract is published.",
                             "The content of each blurb should emphasize the findings of the article and their application in solving real-world problems.",
                             "Avoid describing the level of impact of the study by avoiding words like \"groundbreaking\", \"revolutionary\", and so on.",
                             "Use the tone of a reporter.",
